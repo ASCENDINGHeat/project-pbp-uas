7f13062e6e211d70361551bcf6ddaef364b8e204
ASCENDINGHeat <alfina.jauhar@gmail.com>
2025-11-20 19:36:51 +0700

    initial functionality for the api

---
+++ backend-API/app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facade\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if(Auth::attempt($request->only('eamil', 'password'))){
+            $user = Auth::user();
+
+            $user->tokens()->delete();
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login succesful',
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'Login Failed',
+        ], 401);
+
+    }
+
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'logged out']);
+    }
+
+    public function user(Request $request){
+        return response()->json($request->user(), 200);
+    }
+}
