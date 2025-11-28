fbad3d8106eece8f9a5c8179d2e7a8495dc76c47
ASCENDINGHeat <alfina.jauhar@gmail.com>
2025-11-28 14:06:16 +0700

    added user registration

--- backend-API/routes/api.php
+++ backend-API/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
