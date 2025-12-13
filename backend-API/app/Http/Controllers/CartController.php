a3d66ea2d7316fadf6325458f2024aa66a1f189a
Fawaz <24051204107@mhs.unesa.ac.id>
2025-12-13 15:14:01 +0700

    update checkout dan payment gateway berdasarkan id cart

--- backend-API/app/Http/Controllers/CartController.php
+++ backend-API/app/Http/Controllers/CartController.php
@@ -49,4 +49,69 @@ class CartController extends Controller
             'grand_total' => $grandTotal
         ], 200);
     }
+    public function update(Request $request, $id)
+    {
+        $userId = Auth::id();
+
+        // Validasi: Kita izinkan 0 sekarang (min:0)
+        $request->validate([
+            'quantity' => 'required|integer|min:0'
+        ]);
+
+        // Cari item milik user
+        $cartItem = Cart::where('id', $id)
+            ->where('user_id', $userId)
+            ->first();
+
+        if (!$cartItem) {
+            return response()->json(['message' => 'Item tidak ditemukan'], 404);
+        }
+
+        // --- INI LOGIKA UTAMANYA ---
+        // Jika user mengirim quantity 0, maka hapus barangnya
+        if ($request->quantity <= 0) {
+            $cartItem->delete();
+
+            return response()->json([
+                'status' => 'deleted',
+                'message' => 'Item dihapus dari keranjang karena jumlah 0'
+            ], 200);
+        }
+
+        // Jika quantity > 0, Lanjut Update seperti biasa
+        // Cek Stok dulu
+        if ($cartItem->product->stock < $request->quantity) {
+            return response()->json([
+                'message' => 'Stok tidak mencukupi. Sisa: ' . $cartItem->product->stock
+            ], 400);
+        }
+
+        $cartItem->quantity = $request->quantity;
+        $cartItem->save();
+
+        return response()->json([
+            'status' => 'updated',
+            'message' => 'Jumlah barang berhasil diubah',
+            'data' => $cartItem
+        ], 200);
+    }
+    public function destroy($id)
+    {
+        $userId = Auth::id();
+
+        // Cari cart berdasarkan ID dan User ID (Security)
+        $cartItem = Cart::where('id', $id)
+            ->where('user_id', $userId)
+            ->first();
+
+        if (!$cartItem) {
+            return response()->json(['message' => 'Item tidak ditemukan'], 404);
+        }
+
+        $cartItem->delete();
+
+        return response()->json([
+            'message' => 'Item berhasil dihapus dari keranjang'
+        ], 200);
+    }
 }
