20c7f206c6eb6661cc4e1ba5d9041def35920b8b
= <alfina.jauhar@gmail.com>
2025-12-16 03:30:58 +0700

    edit config

--- frontend-user/svelte.config.js
+++ frontend-user/svelte.config.js
@@ -3,7 +3,9 @@ import adapter from '@sveltejs/adapter-node'; // Change from adapter-auto
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter()
+        adapter: adapter({
+			out: 'build'
+		})
     }
 };
 
